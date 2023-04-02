aa69f03f3849a86b2c4f69481efc82c09d3e16e6
prodigy-sub <djm04092@gmail.com>
2023-04-02 16:57:53 +0900

    add test codes

--- snitch/tests/test_scraper.py
+++ snitch/tests/test_scraper.py
@@ -1,5 +1,21 @@
-def test_always_passes():
-    assert True
+from bs4 import BeautifulSoup
+from snitch.scraper import BaseScraper, NaverNewsScraper
+
+
+def test_url2soup():
+    url = "https://chimhaha.net/"
+    base_scraper = BaseScraper(url)
+    soup = base_scraper.url2soup(url)
+    assert isinstance(soup, BeautifulSoup)
+
+def test_scrap():
+    naver_news_scraper = NaverNewsScraper()
+    articles = naver_news_scraper.scrap("국민은행")
+    assert isinstance(articles, list)
+    assert len(articles) > 0
+    assert articles[0].startswith("https://n.news.naver.com/")
     
-# def test_always_fails():
-#     assert False
+    
+# TODO: should be fixed
+def test_get_articles():
+    assert True
